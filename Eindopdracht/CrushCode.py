8a2d65751070cac6ad9318300077465d1ac7de93
yahyabentje <93516603+yahyabentje@users.noreply.github.com>
2021-11-04 17:35:28 +0100

    Coded crush_code for end task of py

---
+++ Eindopdracht/CrushCode.py
@@ -0,0 +1,25 @@
+# Welkom bij Crush_Code van py.
+
+#Dit is een identifier, waarbij we gaan identificieren om te kijken of je true of false uit komt.
+
+#De identifier wordt gerunt door If statements en input door de interface.
+
+# Als je oud genoegbent krijg je #Crush_Code
+
+#---------------------------------------------------------------------------------------------------------------#
+
+print("Welcom, The minimun Age for Crush_Code is 18")
+
+user_age = int(input("What is your Age? "))
+
+
+import random
+Crush_Code = random.random()
+
+
+if user_age >= 18:
+    print("this is your Crush_Code", Crush_Code)
+else:
+    print("Your not old enough.")
+
+
