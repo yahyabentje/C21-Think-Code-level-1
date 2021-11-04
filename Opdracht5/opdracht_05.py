f1bf0c803af2dff0b4e73bbb2fb63274490375b6
yahyabentje <93516603+yahyabentje@users.noreply.github.com>
2021-11-04 17:03:37 +0100

    corrected the code

--- Opdracht5/opdracht_05.py
+++ Opdracht5/opdracht_05.py
@@ -2,17 +2,9 @@
 #elke keer dat je de functie aanroept voeg je een nieuwe game toe als parameter
 #weet je niet meer hoe? Check het solution filmpje op: https://www.linkedin.com/learning/programming-foundations-fundamentals-3/solution-favorite-cities
 
-def favoriete_steden(name):
-    print("mijn favoriete stad =", name)
+def favoriete_top_drie(name):
+    print("mijn top drie =", name)
 
-favoriete_steden("noordpool")
-
-def favoriete_eten(name):
-    print("mijn eten=", name)
-
-favoriete_eten("Pizza")
-
-def favorie_club(name):
-    print("Mijn clubie =", name)
-
-favorie_club("Ajax")
+favoriete_top_drie("noordpool")
+favoriete_top_drie("Pizza")
+favoriete_top_drie("Ajax")
